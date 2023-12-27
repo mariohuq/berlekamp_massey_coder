5d40c73307d678d1f99cbf90a05fafd968621296
Kvazistam <114609983+Kvazistam@users.noreply.github.com>
2023-12-27 04:15:26 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Kursovaya'
+copyright = '2023, Kvazistam'
+author = 'Kvazistam'
 
 release = '0.1'
 version = '0.1.0'
